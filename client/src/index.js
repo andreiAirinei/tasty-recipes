37fa76d98b8d163da4ddb38383ab929bb02c5e5d
andreiAirinei <andreiAirinei@users.noreply.github.com>
2020-04-10 15:29:51 +0100

    Implemented react-bootstrap & bootswatch themes

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import 'bootswatch/dist/minty/bootstrap.min.css';
+import './styles/app.scss';
 
 ReactDOM.render(
   <React.StrictMode>
