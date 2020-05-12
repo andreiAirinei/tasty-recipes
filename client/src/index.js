fe1c15ebf8066808c6f6817ff7a349fe48b211ae
andreiAirinei <andreiAirinei@users.noreply.github.com>
2020-05-12 09:37:36 +0100

    Changed BrowserRouter with HashRouter because of bugs after build

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+// HashRouter, BrowserRouter, MemoryRouter
+import { HashRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import ScrollToTop from './components/layout/ScrollToTop';
@@ -11,14 +12,14 @@ import './styles/app.scss';
 import store from './redux/store';
 
 ReactDOM.render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <Router>
-      <ScrollToTop>
-        <App />
-      </ScrollToTop>
-    </Router>
-  </Provider>,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
+      </Router>
+    </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
